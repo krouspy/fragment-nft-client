48e0126db3bd16961b392dd7db30c76e8ee71cf6
krouspy <knjlau1@gmail.com>
2020-03-02 14:29:43 +0100

    add ERC721MetadataMintable address

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   serverURI: "https://fragment-nft-server.herokuapp.com",
   fragmentClaimerAddress: "0xEE30651c467d40a1cF09011168e3c21c7ae7a59c",
-  ERC721Address: "0x5c3d2055C44c840b9B433f1B5d117FA90cDfcE30"
+  ERC721MetadataMintableAddress: "0x5c3d2055C44c840b9B433f1B5d117FA90cDfcE30"
 };
