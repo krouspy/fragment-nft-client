0601894a49f85c1644d51c2099cf050ab8963ae4
krouspy <knjlau1@gmail.com>
2020-03-03 09:56:34 +0100

    remove link

--- src/components/Drawer/ListItems/ListItems.jsx
+++ src/components/Drawer/ListItems/ListItems.jsx
@@ -6,7 +6,6 @@ import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import HomeIcon from "@material-ui/icons/Home";
 import InfoIcon from "@material-ui/icons/Info";
-import AppIcon from "@material-ui/icons/Apps";
 
 const linkStyle = {
   textDecoration: "none",
@@ -17,8 +16,10 @@ const iconStyle = {
   color: "#ffffff"
 };
 
+/*
 const path =
   "/claim/0?uri=QmbgVT1Q1pvQiCa8XgHLfYVNZu9U1FEKhhgJbJMV3sNxfT&sig=0x5399fb8ac16970cea342fa56a01d10df2f06fb4391dc69763c0e6ea5af84f37b16d1f98eb93c726d782231005ef86d74c5bd97b348a23ad58389cbf0e55a8bba1c";
+*/
 
 export const ListItems = (
   <List>
@@ -28,14 +29,6 @@ export const ListItems = (
           <HomeIcon style={iconStyle} />
         </ListItemIcon>
         <ListItemText primary="Home" />
-      </ListItem>
-    </Link>
-    <Link to={path} style={linkStyle}>
-      <ListItem button>
-        <ListItemIcon>
-          <AppIcon style={iconStyle} />
-        </ListItemIcon>
-        <ListItemText primary="Claim" />
       </ListItem>
     </Link>
     <Link to="/what" style={linkStyle}>
