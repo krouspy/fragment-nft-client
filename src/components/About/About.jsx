f8866872c99d8b1bd5a661c411db0f15c8cae7a1
krouspy <knjlau1@gmail.com>
2020-03-03 03:47:12 +0100

    modify content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,46 +1,74 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    marginTop: 150
+  },
+  title: {
+    fontWeight: "bold"
+  },
+  warning: {
+    color: "red",
+    fontStyle: "italic"
+  }
+}));
 
 export const About = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>This is you.</h1>
-      <p>
-        The fragment you are holding is part of a picture that was taken a year
-        ago in Paris, during ETHCC2019. Most projects, people, ideas that
-        influence you on a daily basis to work on Ethereum are in it. The
-        complete artwork is made of hundreds of small paths, each one
-        independant from the other; with each one having its own constraints,
-        objectives, turning points and achievements.
-        <br />
-        Taken individually, they are nothing more than themselves; but if you
-        take a step back, they are part of a bigger whole that exceeds the sum
-        of them.
-      </p>
-      <h1>Some cool stats</h1>
-      <p>
-        - The entire artwork is composed of 432 fragments - The total path
-        visits 5,474,296 points. It took 18 days, 4h, 30mn and 38s of
-        calculation time to calculate the optimal-ish path to visit all the
-        points - The full artwork will be about 5.64m in width and 3.24 in
-        height. Let’s coordinate on #fragments to assemble it! - The printed
-        path is a total of 22,947,362mm, or just over 22km! - More on
-        https://fragment-nft.herokuapp.com/what
-      </p>
-      <h1>Claim your associated NFT!</h1>
-      <p>
-        This fragment belongs to the owner of token recorded on contract
-        0x5c3d2055c44c840b9b433f1b5d117fa90cdfce30 on the Ethereum network.
-        Follow the link embedded on the QR code here. You’ll need a Metamask
-        enabled browser. You may also claim it manually - more info on channel
-        #fragments on ETHCC Slack Careful! Anyone with access to this data can
-        claim the token! Token number : URI: Minter signature to claim the
-        token:
-      </p>
-      <p>
-        Warning! This NFT may be claimed up until March 31st, 2020. After that,
-        we may claim/auction/destroy it. Or not. We’ll see. Just don’t expect
-        anything if you do not claim it. Project by Henri Lieutaud & Kenji Lau
-      </p>
-    </div>
+    <Container className={classes.root}>
+      <Typography component="h6" className={classes.title} gutterBottom>
+        <ul>
+          <li>
+            TL;PL
+            <br />
+            Each individual frame is a physical representation of a mathematical
+            artifact that is ubiquitous in our daily lives, without us realizing
+            it.
+          </li>
+          <li>
+            Finding paths
+            <br />
+            The original image is converted to black and white and is then
+            transformed into a list of points that the path must visit. For
+            darker parts of the image, there are more points to visits.
+          </li>
+          <li>
+            Optimising
+            <br />
+            Then the path needs to visit all the points. To do this optimally,
+            it is then a question of solving what is called “the travelling
+            salesman problem” or TSP. Imagine a salesman who wants to visit a
+            group of 20 cities. How to find the ideal route, which will allow
+            him to spend as little time as possible on the road?
+          </li>
+          <li>
+            The itinerant traveler
+            <br />
+            The description of this problem can be identified as early as the
+            19th century, and its first formulation in mathematical terms dates
+            from the 1960s. During the 1970s, 80s and 90s, many brilliant
+            scientists worked on the subject. Indeed, this is a general
+            optimization problem, which appears very regularly in the design of
+            complex systems.
+          </li>
+          <li>
+            In your everyday life
+            <br />
+            Today, TSP solving algorithms are still a thriving area of
+            ​​mathematical research. They are present in most of the information
+            systems on which you depend on a daily basis. These algorithms
+            allows your power grid to deliver electricity, Waze to transport you
+            to Paris, Airbus to build its planes ...
+          </li>
+        </ul>
+      </Typography>
+    </Container>
   );
 };
